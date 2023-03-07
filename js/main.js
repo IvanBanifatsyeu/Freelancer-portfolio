9b4f58428bec254546bc57a2c3ffdeba481505e9
IvanBanifatsyeu <gavrik2010@yahoo.com>
2023-03-07 20:40:26 +0300

    added dark mode

--- js/main.js
+++ js/main.js
@@ -1,6 +1,52 @@
-'use strict'
-const btnDarkMode = document.querySelector('.dark-mode-btn')
+"use strict";
+const btnDarkMode = document.querySelector(".dark-mode-btn");
 
-btnDarkMode.onclick = function  (){
-    btnDarkMode.classList.toggle('dark-mode-btn--active')
+// 1 Проверка темной темы на уровне системных настроек
+if (
+	window.matchMedia &&
+	window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+	console.log("match");
+	console.log(window);
+	btnDarkMode.classList.add("dark-mode-btn--active");
+	document.body.classList.add("dark");
 }
+
+// 2  Проверка темной темы в LocalStorage
+if (localStorage.getItem("darkMode") === "dark") {
+	btnDarkMode.classList.add("dark-mode-btn--active");
+	document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") === "light") {
+	btnDarkMode.classList.remove("dark-mode-btn--active");
+	document.body.classList.remove("dark");
+}
+
+// Если меняются системные настройки, менем тему
+
+window
+	.matchMedia("(prefers-color-scheme: dark)")
+	.addEventListener("change", (event) => {
+		const newColorScheme = event.matches ? "dark" : "light";
+
+		if (newColorScheme === "dark") {
+			btnDarkMode.classList.add("dark-mode-btn--active");
+			document.body.classList.add("dark");
+            localStorage.setItem("darkMode", "dark");
+		} else {
+			btnDarkMode.classList.remove("dark-mode-btn--active");
+			document.body.classList.remove("dark");
+            localStorage.setItem("darkMode", "light");
+		}
+	});
+
+// Включение ночного режима по кнопке
+btnDarkMode.onclick = function () {
+	btnDarkMode.classList.toggle("dark-mode-btn--active");
+	const isDark = document.body.classList.toggle("dark");
+
+	if (isDark) {
+		localStorage.setItem("darkMode", "dark");
+	} else {
+		localStorage.setItem("darkMode", "light");
+	}
+};
